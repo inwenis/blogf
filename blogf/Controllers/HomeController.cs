f220e8679e1750fc3d51a0f119d427fff4ccdb42
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-06-12 14:29:14 +0200

    write to log when someone requests the blog

--- blogf/Controllers/HomeController.cs
+++ blogf/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,20 @@ namespace blogf.Controllers
     {
         public IActionResult Index()
         {
+            var message = DateTimeOffset.UtcNow.ToString("O") + " : someone just requested the blog\n";
+            var headers = HttpContext.Request.Headers;
+            var headersAsString = headers.Select(x => x.Key + " " + x.Value);
+
+            var logFilePath = @"c:\stuff\the_only_log_file.txt";
+            if (!System.IO.File.Exists(logFilePath))
+            {
+                var fileStream = System.IO.File.Create(logFilePath);
+                fileStream.Dispose();
+            }
+
+            System.IO.File.AppendAllText(logFilePath, message);
+            System.IO.File.AppendAllLines(logFilePath, headersAsString);
+
             var rootDir = new DirectoryInfo(@"c:\stuff\content");
             var fileInfos = rootDir.GetFiles("*.txt");
             var posts = fileInfos.Select(x => System.IO.File.ReadAllText(x.FullName));
